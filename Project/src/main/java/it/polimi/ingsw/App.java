8020559fc59f8f501df3648ce9d3da325611ae77
BorghettiMatteo <matteo1.borghetti@gmail.polimi.it>
2020-03-28 21:16:30 +0100

    prova per intellij su ubuntu2

--- Project/src/main/java/it/polimi/ingsw/App.java
+++ Project/src/main/java/it/polimi/ingsw/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Ciao Mondo Alieno!" );
+        System.out.println( "Ciao Mondo Alieno Lontano!" );
     }
 }

5980c8d4a386739f0fa0bb06153033caa037e3fe
AneseMarco <61920704+AneseMarco@users.noreply.github.com>
2020-04-08 16:41:06 +0100

    Implemented other Build strategies

--- Project/src/main/java/it/polimi/ingsw/BuildBeforeAndAfterMoveConcrete.java
+++ Project/src/main/java/it/polimi/ingsw/BuildBeforeAndAfterMoveConcrete.java
@@ -3,6 +3,9 @@ package it.polimi.ingsw;
 public class BuildBeforeAndAfterMoveConcrete implements BuildBlockStrategy {
     @Override
     public void buildBlock(int blockX, int blockY, Turn turn) {
-
+        if ((turn.getCurrentPlayer().getPlayerGod().getEffect().getStatus()) && (!(turn.getMatch().getCell(blockX, blockY).getBuildingLevel()).equals(Level.DOME))) {
+            Level nextLevel = (turn.getMatch().getCell(blockX, blockY).getBuildingLevel()).getNext();
+            turn.getMatch().getCell(blockX, blockY).setBuildingLevel(nextLevel);
+        }
     }
 }
--- Project/src/main/java/it/polimi/ingsw/BuildTwiceSameCellConcrete.java
+++ Project/src/main/java/it/polimi/ingsw/BuildTwiceSameCellConcrete.java
@@ -3,6 +3,10 @@ package it.polimi.ingsw;
 public class BuildTwiceSameCellConcrete implements BuildBlockStrategy {
     @Override
     public void buildBlock(int blockX, int blockY, Turn turn) {
-
+        if (!(turn.getMatch().getCell(blockX, blockY).getBuildingLevel()).equals(Level.DOME)) {
+            Level nextLevel = (turn.getMatch().getCell(blockX, blockY).getBuildingLevel()).getNext();
+            turn.getMatch().getCell(blockX, blockY).setBuildingLevel(nextLevel);
+        }
+        /* need another strategy for the second build */
     }
 }

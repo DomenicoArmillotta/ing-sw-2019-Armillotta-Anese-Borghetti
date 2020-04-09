14159a1c892bf3bef09003ebfce5fdacfb01b4f4
BorghettiMatteo <matteo1.borghetti@gmail.polimi.it>
2020-04-09 17:14:55 +0200

    adding basic select concrete

--- Project/src/main/java/it/polimi/ingsw/BasicSelectOptionsConcrete.java
+++ Project/src/main/java/it/polimi/ingsw/BasicSelectOptionsConcrete.java
@@ -3,6 +3,12 @@ package it.polimi.ingsw;
 public class BasicSelectOptionsConcrete implements SelectOptionsStrategy {
     @Override
     public Worker select(Turn turn) {
-        return null;
+        //allora il controller mi passa tutte le cose gia paraste e io devo solo;
+        //digerirle;
+        //some input mi aspetto che ciò che passa sia == a quello che il model;
+        //ha in serbo;
+        int i=0,j=0;
+
+        return turn.getMatch().getMap()[i][j].getWorkerOnCell();
     }
 }

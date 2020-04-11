899c5b8cf279cd7fe67cb880e2647bb8bc3fb0c7
BorghettiMatteo <matteo1.borghetti@gmail.polimi.it>
2020-04-12 00:31:46 +0200

    committing ShowSelectOptionsBasicConcrete and his test class

--- Project/src/main/java/it/polimi/ingsw/ShowSelectOptionsBasicConcrete.java
+++ Project/src/main/java/it/polimi/ingsw/ShowSelectOptionsBasicConcrete.java
@@ -1,11 +1,52 @@
 package it.polimi.ingsw;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShowSelectOptionsBasicConcrete implements ShowSelectOptionsStrategy {
+
+    private boolean lookAround(Worker worker,Match match) {
+        Cell workersCell = worker.getCurrentPosition();
+        int i, j;
+        boolean canSelectWorker = false;
+        int tempX = worker.getCurrentPosition().getX();
+        int tempY = worker.getCurrentPosition().getY();
+        int deltaLevel;
+
+
+        for (i = tempX - 1; i < tempX + 2; i++) {
+            for (j = tempY +1; j > tempY - 2; j--) {
+                if ((i >= 0 && i < 5) && (j >= 0 && j <5)) {
+                    if (match.getMap()[i][j].getWorkerOnCell() == null && match.getMap()[i][j].getBuildingLevel() != Level.DOME) {
+                        deltaLevel = (match.getMap()[i][j].getBuildingLevel().ordinal()) - (worker.getCurrentPosition().getBuildingLevel().ordinal());
+                        if (deltaLevel < 2) {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+        }
+        return false;
+    }
     @Override
     public List<Cell> showSelect(Match match) {
-        //faccio il controllo per i giocatori murati
-    return null;
+        Player currPlayer = match.getCurrentTurn().getCurrentPlayer();
+        List<Cell> tempCells = new ArrayList();
+        int i,j;
+        for(i=0;i<5;i++){
+            for(j=0;j<5;j++){
+                if(match.getMap()[i][j].getWorkerOnCell()!=null&&match.getMap()[i][j].getWorkerOnCell().getOwner()==currPlayer){
+                    if(lookAround(match.getMap()[i][j].getWorkerOnCell(), match)){
+                        tempCells.add(match.getMap()[i][j]);
+                    }
+                }
+            }
+        }
+
+        if(tempCells.size()==0) {
+            System.out.println("hai perso per favore chiamiamo la routine che fa finire questo strazio");
+        }
+    return tempCells;
     }
 }
--- Project/src/test/java/it/polimi/ingsw/ShowSelectOptionsBasicConcreteTest.java
+++ Project/src/test/java/it/polimi/ingsw/ShowSelectOptionsBasicConcreteTest.java
@@ -3,7 +3,7 @@ package it.polimi.ingsw;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,8 +16,7 @@ public class ShowSelectOptionsBasicConcreteTest {
         Player player2 = new Player("Pietro");
         Player player3 = new Player("Domenico");
         List<Player> playerQueue = new ArrayList<>();
-        List<Worker> workersQueue = new ArrayList<>();
-        )
+        List<Cell> workersCellsQueue = new ArrayList<>();
 
         playerQueue.add(player1);
         playerQueue.add(player2);
@@ -29,6 +28,37 @@ public class ShowSelectOptionsBasicConcreteTest {
         myMatch.createMap();
         myMatch.createGodList();
 
+        player1.setCurrentMatch(myMatch);
+        player1.setPlayerGod(myMatch.getGodList().get(0)); // apollo
+        //creo un dummy worker in 0,0 di player1
+        player1.initFirstWorker(0,0);
+        player1.initSecondWorker(4,4);
+        //creo un turno dummy
+        myMatch.startFirstTurn(playerQueue);
+
+        workersCellsQueue = myMatch.getCurrentTurn().getCurrentPlayer().getPlayerGod().getEffect().doShowSelectOptions(myMatch);
+        assertEquals(player1.getFirstWorker().getCurrentPosition(),workersCellsQueue.get(0));
+        assertEquals(player1.getSecondWorker().getCurrentPosition(),workersCellsQueue.get(1));
+    }
+    @Test
+    public void lostgameTest(){
+        //voglio che i due worker istanziati sia == a quelli ritornati dalla show
+        //mi aspetto che ciò che torna dal controller sia == a ciò storato nel model
+        Player player1 = new Player("Marco");
+        Player player2 = new Player("Pietro");
+        Player player3 = new Player("Domenico");
+        List<Player> playerQueue = new ArrayList<>();
+        List<Cell> workersCellsQueue = new ArrayList<>();
+
+        playerQueue.add(player1);
+        playerQueue.add(player2);
+        playerQueue.add(player3);
+
+        GameMaster myGameMaster = new GameMaster(3, playerQueue);
+        myGameMaster.createMatch();
+        Match myMatch = myGameMaster.getMatch();
+        myMatch.createMap();
+        myMatch.createGodList();
 
         player1.setCurrentMatch(myMatch);
         player1.setPlayerGod(myMatch.getGodList().get(0)); // apollo
@@ -37,9 +67,11 @@ public class ShowSelectOptionsBasicConcreteTest {
         player1.initSecondWorker(4,4);
         //creo un turno dummy
         myMatch.startFirstTurn(playerQueue);
-        //asserisco che la show ritoni la lista con i worker di player1 giusti
-        workersQueue = myMatch.getCurrentTurn().getCurrentPlayer().getPlayerGod().getEffect().
-        assertEquals(player1.getFirstWorker(),);
-    }
+        myMatch.getMap()[0][1].setBuildingLevel(Level.DOME);
+        myMatch.getMap()[1][1].setBuildingLevel(Level.DOME);
+        myMatch.getMap()[1][0].setBuildingLevel(Level.DOME);
 
+        workersCellsQueue = myMatch.getCurrentTurn().getCurrentPlayer().getPlayerGod().getEffect().doShowSelectOptions(myMatch);
+        assertEquals(1,workersCellsQueue.size());
+    }
 }

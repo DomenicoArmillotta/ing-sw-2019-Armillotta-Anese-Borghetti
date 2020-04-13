efbaafc9f3e7700e10b891003b52ed9d1025f958
AneseMarco <61920704+AneseMarco@users.noreply.github.com>
2020-04-13 17:48:41 +0100

    Pushed SelectRestraintsDontMoveUpConcrete Class

---
+++ Project/src/main/java/it/polimi/ingsw/SelectRestraintsDontMoveUpConcrete.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw;
+
+import java.util.List;
+
+public class SelectRestraintsDontMoveUpConcrete implements SubtractRestraintsStrategy {
+    @Override
+    public List<Cell> doSubtractRestraints(List<Cell> selectOptionsCells) {
+        return null;
+    }
+}

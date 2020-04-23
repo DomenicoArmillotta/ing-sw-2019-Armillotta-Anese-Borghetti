29370a668e3d148ca2af2d7b71946f7ea3c594ff
Domenico Armillotta <61909857+DomenicoArmillotta@users.noreply.github.com>
2020-04-23 17:47:29 +0200

    update test

--- Project/src/main/java/it/polimi/ingsw/model/powertree/Build.java
+++ Project/src/main/java/it/polimi/ingsw/model/powertree/Build.java
@@ -10,11 +10,11 @@ public class Build extends LimitedPower {
     public int doAction(int[] userInput) {
         List<Cell> availableCells = getAvailableCells();
         Cell[][] map = getExecutorPointer().getMap();
+        //cella su cui voglio costruire
         int blockX = userInput[0];
         int blockY = userInput[1];
-
         if (availableCells.contains(map[blockX][blockY])) {
-            map[blockX][blockY].setBuildingLevel(map[blockX][blockY].getBuildingLevel());
+            map[blockX][blockY].setBuildingLevel(map[blockX][blockY].getBuildingLevel().getNext());
             return 0;
         }
 

ab8d4ab924c3bc6649eff30c25d0342dad7f6a14
Domenico Armillotta <61909857+DomenicoArmillotta@users.noreply.github.com>
2020-04-23 18:57:09 +0200

    create WinCondition

--- Project/src/main/java/it/polimi/ingsw/model/powertree/WinCondition.java
+++ Project/src/main/java/it/polimi/ingsw/model/powertree/WinCondition.java
@@ -1,4 +1,16 @@
 package it.polimi.ingsw.model.powertree;
 
+import it.polimi.ingsw.model.Level;
+import it.polimi.ingsw.model.Worker;
+
+//se mi trovo nella posizione top
 public class WinCondition extends Power {
+    @Override
+    public int doAction(int[] userInput) {
+        Worker selectedWorker = super.getExecutorPointer().getPrevSelect().getSelectedWorker();
+        if((selectedWorker.getPreviousPosition().getBuildingLevel().ordinal()-selectedWorker.getCurrentPosition().getBuildingLevel().ordinal())==-1 && selectedWorker.getCurrentPosition().getBuildingLevel()== Level.TOP ){
+            return 0;
+        }else
+            return -1;
+    }
 }
--- Project/src/main/java/it/polimi/ingsw/model/powertree/WinIfTwoLevelsDown.java
+++ Project/src/main/java/it/polimi/ingsw/model/powertree/WinIfTwoLevelsDown.java
@@ -1,4 +1,17 @@
 package it.polimi.ingsw.model.powertree;
 
-public class WinIfTwoLevelsDown {
+import it.polimi.ingsw.model.Level;
+import it.polimi.ingsw.model.Worker;
+
+//pan super.doaction
+public class WinIfTwoLevelsDown extends WinCondition{
+    @Override
+    public int doAction(int[] userInput) {
+        Worker selectedWorker = super.getExecutorPointer().getPrevSelect().getSelectedWorker();
+        super.doAction(userInput);
+        if((selectedWorker.getPreviousPosition().getBuildingLevel().ordinal()-selectedWorker.getCurrentPosition().getBuildingLevel().ordinal())==2  ){
+            return 0;
+        }else
+            return -1;
+    }
 }

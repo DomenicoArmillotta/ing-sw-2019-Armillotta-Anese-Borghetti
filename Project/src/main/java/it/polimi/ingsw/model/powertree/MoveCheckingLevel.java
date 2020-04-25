21a5fc4e2e0b99d31ccc1b49ee4bb5b775b6a3f2
BorghettiMatteo <matteo1.borghetti@gmail.polimi.it>
2020-04-25 11:45:40 +0200

    pushing MoveCheckingLevel

--- Project/src/main/java/it/polimi/ingsw/model/powertree/MoveCheckingLevel.java
+++ Project/src/main/java/it/polimi/ingsw/model/powertree/MoveCheckingLevel.java
@@ -1,4 +1,43 @@
 package it.polimi.ingsw.model.powertree;
 
-public class MoveCheckingLevel {
+import it.polimi.ingsw.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoveCheckingLevel extends Move{
+    /*voglio fare una copia della powerList e scorrerla fin che non trovo la move e quindi tornare indietro
+    di un passo*/
+    private void computeInjectionPowerIndex(Player player){
+        List<Power> cachedPowerList =  new ArrayList<>();
+        cachedPowerList.addAll(player.getPlayerGod().getPowerList());
+        Power injectionPtr;
+        Power selectListPtr = player.getPlayerGod().getSelectList().get(0);
+        injectionPtr = player.getPlayerGod().getFindAvailableCellsList().get(0);
+        int i;
+
+        for(i=0 ; cachedPowerList.get(i)!= selectListPtr; i++){
+            ;
+        }
+        cachedPowerList.add(i,new FindAvailableCellsMoveButDontMoveUp());
+        //ho aggiornato la list power ma non la FindAvailableCells list;
+        player.getPlayerGod().getFindAvailableCellsList().add(i-1,new FindAvailableCellsMoveButDontMoveUp());
+        player.getPlayerGod().setPowerList(cachedPowerList);
+        System.out.println("dddd");
+    }
+
+    @Override
+    public int doAction(int[] userInput) {
+        super.doAction(userInput);
+        /*
+        devo computare ilchanging di startegy
+         */
+        Worker movedWorker = super.getExecutorPointer().getMap()[userInput[0]][userInput[1]].getWorkerOnCell();
+        if(movedWorker.getCurrentPosition().getBuildingLevel().ordinal() - movedWorker.getPreviousPosition().getBuildingLevel().ordinal() == 1){
+            computeInjectionPowerIndex(super.getExecutorPointer().getNextPlayer());
+            computeInjectionPowerIndex(super.getExecutorPointer().getPrevPlayer());
+            return 0;
+        }
+        return 0;
+    }
 }
---
+++ Project/src/test/java/it/polimi/ingsw/model/powertree/FindAvailableCellAppendTest.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw.model.powertree;
+
+import it.polimi.ingsw.model.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FindAvailableCellAppendTest {
+    @Test
+    public void correctlyAppendFindAvailableCellsDontMoveUp(){
+        Player player1 = new Player("Marco");
+        Player player2 = new Player("Pietro");
+        Player player3 = new Player("Domenico");
+        List<Player> playerQueue = new ArrayList<>();
+        playerQueue.add(player1);
+        playerQueue.add(player2);
+        playerQueue.add(player3);
+        GameMaster gameMaster = new GameMaster(playerQueue, 3);
+        gameMaster.createGodList();
+        player1.setPlayerGod(gameMaster.getGodList().get(God.ATHENA.ordinal()));
+        player2.setPlayerGod(gameMaster.getGodList().get(God.MORTAL.ordinal()));
+        player3.setPlayerGod(gameMaster.getGodList().get(God.MORTAL.ordinal()));
+        GodCard godCard1 = player1.getPlayerGod();
+        GodCard godCard2 = player2.getPlayerGod();
+        GodCard godCard3 = player3.getPlayerGod();
+        ActionExecutor actionExecutor = gameMaster.getActionExecutor();
+        actionExecutor.createMap();
+        Cell[][] map = actionExecutor.getMap();
+        map[1][0].setBuildingLevel(Level.BASE);
+        player1.workersSetup(0, 0, 1, 1);
+        player2.workersSetup(0, 4, 2, 1);
+        player3.workersSetup(1, 2, 4, 4);
+
+        int[] userInput = new int[2];
+        userInput[0] = 0;
+        userInput[1] = 0;
+
+
+        assertEquals(actionExecutor.getNextPower().doAction(userInput), 0);//find cell select
+        assertEquals(actionExecutor.getNextPower().doAction(userInput),0);//select
+        userInput[0] = 1;
+        userInput[1] = 0;
+        assertEquals(actionExecutor.getNextPower().doAction(userInput),0);//find cell move
+        assertEquals(true, true);
+    }
+
+}

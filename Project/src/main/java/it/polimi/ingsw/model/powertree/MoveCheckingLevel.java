3f9c4d699ed23ab8e9114040a706c7dd91fc090e
BorghettiMatteo <matteo1.borghetti@gmail.polimi.it>
2020-04-25 12:09:59 +0200

    correcting pointer error in MoveCheckingLevel

--- Project/src/main/java/it/polimi/ingsw/model/powertree/MoveCheckingLevel.java
+++ Project/src/main/java/it/polimi/ingsw/model/powertree/MoveCheckingLevel.java
@@ -9,8 +9,7 @@ public class MoveCheckingLevel extends Move{
     /*voglio fare una copia della powerList e scorrerla fin che non trovo la move e quindi tornare indietro
     di un passo*/
     private void computeInjectionPowerIndex(Player player){
-        List<Power> cachedPowerList =  new ArrayList<>();
-        cachedPowerList.addAll(player.getPlayerGod().getPowerList());
+        List<Power> cachedPowerList = player.getPlayerGod().getPowerList();
         Power injectionPtr;
         Power selectListPtr = player.getPlayerGod().getSelectList().get(0);
         injectionPtr = player.getPlayerGod().getFindAvailableCellsList().get(0);
@@ -19,18 +18,18 @@ public class MoveCheckingLevel extends Move{
         for(i=0 ; cachedPowerList.get(i)!= selectListPtr; i++){
             ;
         }
+        player.getPlayerGod().getFindAvailableCellsList().add(i,new FindAvailableCellsMoveButDontMoveUp());
         cachedPowerList.add(i,new FindAvailableCellsMoveButDontMoveUp());
         //ho aggiornato la list power ma non la FindAvailableCells list;
-        player.getPlayerGod().getFindAvailableCellsList().add(i-1,new FindAvailableCellsMoveButDontMoveUp());
         player.getPlayerGod().setPowerList(cachedPowerList);
-        System.out.println("dddd");
+
     }
 
     @Override
     public int doAction(int[] userInput) {
         super.doAction(userInput);
         /*
-        devo computare ilchanging di startegy
+        devo computare il changing di startegy
          */
         Worker movedWorker = super.getExecutorPointer().getMap()[userInput[0]][userInput[1]].getWorkerOnCell();
         if(movedWorker.getCurrentPosition().getBuildingLevel().ordinal() - movedWorker.getPreviousPosition().getBuildingLevel().ordinal() == 1){

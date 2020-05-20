d4ea93a03b00d322200ede0829fbe9ec32611679
Domenico Armillotta <10582521@polimi.it>
2020-05-20 17:53:34 +0200

    update cli

---
+++ Project/src/main/java/it/polimi/ingsw/client/proxymodel/GodCards.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.client.proxymodel;
+
+public class GodCards {
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

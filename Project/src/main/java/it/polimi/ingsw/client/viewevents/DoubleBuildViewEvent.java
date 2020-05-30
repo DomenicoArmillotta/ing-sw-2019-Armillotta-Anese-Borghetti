10148d92d183ce65b3575ffab5b17656f44f445a
BorghettiMatteo <matteo1.borghetti@mail.polimi.it>
2020-05-30 20:26:37 +0200

    fixing Hepaestus bugs and updating BuildOnSameCell class with the logic necessary to accept the same coordinates and go forward without getting stuck

---
+++ Project/src/main/java/it/polimi/ingsw/client/viewevents/DoubleBuildViewEvent.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.client.viewevents;
+
+import it.polimi.ingsw.client.proxymodel.ProxyModel;
+
+public class DoubleBuildViewEvent extends ViewEvent {
+    String promptText;
+    /* aggiungere livello */
+
+    public DoubleBuildViewEvent(String promptText) {
+        this.promptText = promptText;
+    }
+
+    public void viewEventMethod() {
+        System.out.println("\u001B[36m" + ProxyModel.instance().getTurn().getCurrentPlayer().getName() + "\u001B[0m"+", "+promptText);
+        System.out.println("Answer by typing "+"\u001B[33m"+"bool"+"\u001B[0m"+" followed by "+"\u001B[33m"+"true"+"\u001B[0m"+" or "+"\u001B[33m"+"false"+"\u001B[0m"+".");
+    }
+}

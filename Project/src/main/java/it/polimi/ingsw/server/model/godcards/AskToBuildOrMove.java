b9ff42eab438bc5e37fb74cca01950d84b11151d
AneseMarco <61920704+AneseMarco@users.noreply.github.com>
2020-06-04 16:26:58 +0100

    Written BooleanRequestStrategy in AskToBuildOrMove

--- Project/src/main/java/it/polimi/ingsw/server/model/godcards/AskToBuildOrMove.java
+++ Project/src/main/java/it/polimi/ingsw/server/model/godcards/AskToBuildOrMove.java
@@ -1,9 +1,18 @@
 package it.polimi.ingsw.server.model.godcards;
 
 import it.polimi.ingsw.server.controller.Controller;
+import it.polimi.ingsw.server.model.ActionExecutor;
+import it.polimi.ingsw.server.model.mvevents.eventbeans.NoUpdatesEventBean;
+import it.polimi.ingsw.server.virtualview.network.EventsBuffer;
 
 public class AskToBuildOrMove implements BooleanRequestAction{
     @Override
     public void BooleanRequestStrategy(Controller controller, Boolean booleanChoice) {
+        /* true = costruisci; false = non costruisci */
+        if(!booleanChoice) {
+            ActionExecutor.instance().getNextPower().doAction(null);
+        } else {
+            EventsBuffer.instance().setLastEventBean(new NoUpdatesEventBean());
+        }
     }
 }
